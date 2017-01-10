88b95adf661d60a65d8274354e3094495c9c727d
12Me21 <12Me21.MC@gmail.com>
2017-01-10 18:58:28 -0500

    is fix?

--- syntax/script.js
+++ syntax/script.js
@@ -34,6 +34,7 @@ var applySyntaxHighlighting = (function()
 		find(false     ,/(?:\d*\.)?\d+(?=E)/igm,text,list); //must be before number. Causes numbers using E without a digit after to not be highlighted
 		find(false     ,/&amp;&amp;|&&/igm,text,list); //must be before number. Makes sure code like 7&&HEAD is not interpreted as a hex number.
 		find("number"  ,/-?(?:(?:\d*\.(?:\d+(?:E-?\d+)?)?)|(?:\d+(?:E-?\d+)?))#?|(?:&amp;|&)H[\dA-F]+|(?:&amp;|&)B[01]+|#[A-Z\d]+/igm,text,list);
+		find("number"  ,/\.\d+/igm,text,list); //after number I guess.
 		find("label"   ,/@[0-9A-Z_]*/igm,text,list);
 		find("string"  ,/\".*?(?:\"|$)/igm,text,list);
 		find("comment" ,/'[^'\n\r]*/igm,text,list);
