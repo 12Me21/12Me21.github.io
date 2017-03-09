4f4c66dd341b3e5c67bd93880b64da1d28f7b89b
12Me21 <12Me21.MC@gmail.com>
2017-03-09 14:52:55 -0500

    Create script.js

---
+++ paste/script.js
@@ -0,0 +1,7 @@
+function paste(element, callback) {
+	element.addEventListener("load", function() {
+		var image = element.getElementsByTagName("img")[0]
+		if (image) callback.bind(image)()
+		element.innerHTML=""
+	}, true)
+}
