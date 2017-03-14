a1bbd83d32d74ef6a0a7b5247f9b954c13958541
12Me21 <12Me21.MC@gmail.com>
2017-03-14 12:22:21 -0400

    IE support
    replace image.remove() with element.removeChild(image)

--- paste/script.js
+++ paste/script.js
@@ -3,7 +3,7 @@ function paste(element, callback) {
 		var image = element.getElementsByTagName("img")[0]
 		if (image) {
 			callback.bind(image)()
-			image.remove()
+			element.removeChild(image)
 		}
 	}, true)
 }
