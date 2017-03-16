feca53dde4413b54511c42943c4148fa504cd153
12Me21 <12Me21.MC@gmail.com>
2017-03-16 09:27:08 -0400

    works in chrome now

--- paste/script.js
+++ paste/script.js
@@ -1,9 +1,29 @@
-function paste(element, callback) {
-	element.addEventListener("load", function() {
-		var image = element.getElementsByTagName("img")[0]
-		if (image) {
-			callback.bind(image)()
-			element.removeChild(image)
+function imagePaste(element, callback) {
+	var foundClipboardData = false
+	//Use clipboardData when possible
+	//(Works in Firefox, Chrome, and Edge)
+	element.addEventListener("paste", function(event) {
+		var clipboardData = event.clipboardData
+		if (clipboardData && clipboardData.types[0] == "Files") {
+			console.log("Using clipboardData")
+			foundClipboardData = true
+			var image = new Image()
+			image.onload = callback
+			image.src = URL.createObjectURL(clipboardData.items[0].getAsFile())
+		}
+	}, false)
+	//If browser doesn't support clipboardData, or if the user pasted a file, use this function
+	//(Works in all browsers except Chrome)
+	//Also used in firefox to remove the pasted image element.
+	element.addEventListener("load", function(event) {
+		var pastedImage = event.target
+		if (pastedImage instanceof HTMLImageElement) {
+			console.log("Found image element")
+			var onloadResult = element.removeChild(pastedImage)
+			if (!foundClipboardData) {
+				console.log("Using pasted image element")
+				callback.bind(onloadResult)()
+			}
 		}
 	}, true)
 }
