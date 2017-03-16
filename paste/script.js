9bc5b37cda255eefcd3762bd14732ed9e651bd23
12Me21 <12Me21.MC@gmail.com>
2017-03-16 15:19:30 -0400

    Update script.js

--- paste/script.js
+++ paste/script.js
@@ -4,23 +4,32 @@ function imagePaste(element, callback) {
 	//Use clipboardData when possible
 	//(Works in Firefox, Chrome, and Edge)
 	element.addEventListener("paste", function(event) {
-		var clipboardData = event.clipboardData
-		if (clipboardData && clipboardData.types[0] == "Files") {
-			console.log("Using clipboardData")
-			foundClipboardData = true
-			var image = new Image()
-			image.onload = callback
-			image.src = URL.createObjectURL(clipboardData.items[0].getAsFile())
+    var clipboardData = event.clipboardData
+    if (clipboardData && clipboardData.types) {
+		//Find the first file. Can't use indexOf since it's a DOMStringList NOT an Array.
+		for (var i=0;i<clipboardData.types.length;i++) 
+			if (clipboardData.types[i] == "Files") {
+				console.log("Using clipboardData")
+				foundClipboardData = true
+				var image = new Image()
+				image.onload = callback
+				image.src = URL.createObjectURL(clipboardData.items[i].getAsFile())
+				break
+			}
 		}
 	}, false)
-	//If browser doesn't support clipboardData, or if the user pasted a file, use this function
-	//(Works in all browsers except Chrome)
-	//Also used in firefox to remove the pasted image element.
+	//If browser doesn't support clipboardData, this function is used
+	//(Works in all browsers except Chrome and Edge)
+	//Also used in Firefox to remove the pasted image element.
 	element.addEventListener("load", function(event) {
 		var pastedImage = event.target
 		if (pastedImage instanceof HTMLImageElement) {
-			console.log("Found image element")
-			var onloadResult = element.removeChild(pastedImage)
+			try{
+				var onloadResult = element.removeChild(pastedImage)
+			} catch(e){
+				console.log("Error: Image randomly disappeared!")
+				return
+			}
 			if (!foundClipboardData) {
 				console.log("Using pasted image element")
 				callback.bind(onloadResult)()
