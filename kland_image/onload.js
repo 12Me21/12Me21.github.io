8de48ee36f9697ab1da94e4507c8b459e5283b28
12Me21 <12Me21.MC@gmail.com>
2017-03-22 12:55:44 -0400

    Create onload.js

---
+++ kland_image/onload.js
@@ -0,0 +1,16 @@
+//paste image
+imagePaste($imageInput, function() {
+	doUpload(this)
+})
+//select file
+imageUpload($fileInput, function() {
+	doUpload(this)
+})
+
+function doUpload(image) {
+	toBlob(image, function(blob) {
+		upload(blob, $bucketInput.value, function() {
+			$resultLink.href = $resultImage.src = this.response
+		})
+	})
+}
