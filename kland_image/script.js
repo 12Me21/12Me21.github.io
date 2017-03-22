2985fc4c5958581f87079de8a79bc0b3fc52de50
12Me21 <12Me21.MC@gmail.com>
2017-03-22 12:57:28 -0400

    general improvements, separated onload parts
    - images uploaded with the file browser are now converted to png or jpeg depending on their size
    - code that should run when the page loads is in a separate file
    - added a function to handle images from the file browser

--- kland_image/script.js
+++ kland_image/script.js
@@ -1,50 +1,52 @@
 //$ prefixed variables are element IDs
 //deal with it
 
-window.onload = function() {
-  var c2d = $canvas.getContext("2d")
-  imagePaste($imageInput, function() {
-    $canvas.width = this.width
-    $canvas.height = this.height
-    c2d.drawImage(this, 0, 0)
-    $canvas.toBlobSmart(function(blob) {
-      upload(blob, $bucketInput.value)
-    })
-  })
-
-  $fileInput.onchange = function() {
-    upload(this.files[0], $bucketInput.value)
-  }
+//get image from file browser
+function imageUpload(element, callback) {
+   element.onchange = function() {
+      var reader = new FileReader()
+      reader.onload = function() {
+         var image = new Image()
+         image.onload = callback
+         image.src = this.result
+      }
+      reader.readAsDataURL(this.files[0])
+   }
 }
 
 //Convert image to blob PNG or JPEG depending on the size
-HTMLCanvasElement.prototype.toBlobSmart = function(callback) {
-  var canvas = this
-  canvas.toBlob(function(blobPng) {
-    if (blobPng.size <= 750000) {
-      console.log("Converted to PNG")
-      callback(blobPng)
-    } else {
-      canvas.toBlob(function(blobJpeg) {
-        console.log("Converted to JPEG")
-        callback(blobJpeg)
-      }, "image/jpeg", 0.9)
-    }
-  })
-}
+var toBlob = (function() {
+   var canvas = document.createElement("canvas")
+   return function(image, callback) {
+      //draw on canvas
+      canvas.width = image.width
+      canvas.height = image.height
+      canvas.getContext("2d").drawImage(image, 0, 0)
+      //get png blob
+      canvas.toBlob(function(blobPng) {
+         if (blobPng.size <= 750000) {
+            console.log("Converted to PNG")
+            callback(blobPng)
+         } else {
+            //get jpeg blob if png is too big
+            canvas.toBlob(function(blobJpeg) {
+               console.log("Converted to JPEG")
+               callback(blobJpeg)
+            }, "image/jpeg", 0.9)
+         }
+      })
+   }
+})()
 
-function upload(blob, bucket) {
-  console.log("Uploading image")
-    //generate form data
-  var data = new FormData();
-  data.append("image", blob, (blob.type == "image/jpeg") ? "blob.jpeg" : "blob.png")
-  data.append("bucket", bucket)
-    //send
-  var xhr = new XMLHttpRequest()
-  xhr.open("POST", "https://kland.smilebasicsource.com/uploadimage")
-  xhr.onload = function(event) {
-    $resultLink.href = event.target.response
-    $resultImage.src = event.target.response
-  }
-  xhr.send(data)
+//upload image to kland
+function upload(blob, bucket, callback) {
+   //generate form data
+   var data = new FormData()
+   data.append("image", blob, blob.type == "image/jpeg" ? "blob.jpeg" : "blob.png")
+   data.append("bucket", bucket)
+   //send
+   var xhr = new XMLHttpRequest()
+   xhr.open("POST", "https://kland.smilebasicsource.com/uploadimage")
+   xhr.onload = callback
+   xhr.send(data)
 }
