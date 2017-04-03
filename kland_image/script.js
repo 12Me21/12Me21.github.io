7c9c1b0dbd15c7c2d5cd9667741bc59c54954743
12Me21 <12Me21.MC@gmail.com>
2017-04-03 11:03:56 -0400

    removed alerts

--- kland_image/script.js
+++ kland_image/script.js
@@ -1,19 +1,15 @@
 //get image from file browser
 function imageUpload(element, callback) {
    element.onchange = function() {
-      //alert("changed")
-      if (HTMLCanvasElement.prototype.toBlob) {
-         var reader = new FileReader()
+        var reader = new FileReader()
          reader.onload = function() {
-            //alert("reader loaded")
+           
             var image = new Image()
             image.onload = callback
             image.src = this.result
       	}
       	reader.readAsDataURL(this.files[0])
-		} else {
-			callback.bind(this.files[0])()
-		}
+	
    }
 }
 
@@ -22,28 +18,23 @@ var toBlob = (function() {
    var canvas = document.createElement("canvas")
    return function(image, callback) {
       if (image instanceof Blob) callback(image)
-		//alert("converting")
       //draw on canvas
       canvas.width = image.width
       canvas.height = image.height
       canvas.getContext("2d").drawImage(image, 0, 0)
-     	if (!HTMLCanvasElement.prototype.toBlob) {
-			alert("Browser does not support canvas.toBlob")
-			return
-		}
+     if (!HTMLCanvasElement.prototype.toBlob) {
+	alert("Browser does not support canvas.toBlob.\nTry using 12Me21.github.io/kland_image/simple")
+	return
+	}
       //get png blob
       canvas.toBlob(function(blobPng) {
-         //alert("tried to PNG")
          if (blobPng.size <= 750000) {
             console.log("Converted to PNG")
-               //alert("png")
             callback(blobPng)
          } else {
             //get jpeg blob if png is too big
             canvas.toBlob(function(blobJpeg) {
-               //alert("tried to jpeg")
                console.log("Converted to JPEG")
-               //alert("jpeg")
                callback(blobJpeg)
             }, "image/jpeg", 0.9)
          }
@@ -53,16 +44,13 @@ var toBlob = (function() {
 
 //upload image to kland
 function upload(blob, bucket, callback) {
-   alert("uploading")
    //generate form data
    var data = new FormData()
    data.append("image", blob, blob.type == "image/jpeg" ? "blob.jpeg" : "blob.png")
    data.append("bucket", bucket)
-   alert("created form data :)")
    //send
    var xhr = new XMLHttpRequest()
    xhr.open("POST", "https://kland.smilebasicsource.com/uploadimage")
    xhr.onload = callback
    xhr.send(data)
-	alert("tried to send?")
 }
