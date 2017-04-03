8640859bdd4dfb17bc14223281cc3c078ae8cea7
12Me21 <12Me21.MC@gmail.com>
2017-04-03 11:02:16 -0400

    removed alerts

--- kland_image/onload.js
+++ kland_image/onload.js
@@ -11,14 +11,9 @@ imageUpload($browseInput, function() {
 })
 
 function doUpload(image) {
-	//alert("do upload")
 	toBlob(image, function(blob) {
-		alert("to blob done")
 		upload(blob, $bucketInput.value, function() {
-			alert("uploaded?")
-			alert(this.response)
 			$linkOutput.href = $imageOutput.src = this.response
-			alert("DONE")
 		})
 	})
 }
