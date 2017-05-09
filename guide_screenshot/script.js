1d9deb09cf851396fe7e94e7c24b349c032393f3
12Me21 <12Me21.MC@gmail.com>
2017-05-09 13:02:13 -0400

    pleeease

--- guide_screenshot/script.js
+++ guide_screenshot/script.js
@@ -15,6 +15,7 @@ window.onload=function(){
 	window.setTimeout(function(){
 		var c2d=$canvas.getContext("2d")
 		c2d.gcls("black")
-		c2d.gputchr($canvas.width,$canvas.height,"test",16,"red")
+		window.location.hash="3"
+		c2d.gputchr($canvas.width/2-((window.location.hash.length-1)*32/2),$canvas.height/2-(32/2),window.location.hash.substr(1),32,"red")
 	},1000)
 }
