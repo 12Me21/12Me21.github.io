b6f627987f4437aa12640df49720f93f89fdcbb2
12Me21 <12Me21.MC@gmail.com>
2017-05-09 13:12:18 -0400

    Update script.js

--- guide_screenshot/script.js
+++ guide_screenshot/script.js
@@ -11,11 +11,21 @@ CanvasRenderingContext2D.prototype.gputchr=function(x,y,text,scale,color){
 	this.fillText(text, x, y+scale);
 }
 
+var urlParams;
+(window.onpopstate = function () {
+    var match,
+        pl     = /\+/g,  // Regex for replacing addition symbol with a space
+        search = /([^&=]+)=?([^&]*)/g,
+        decode = function (s) { return decodeURIComponent(s.replace(pl, " ")); },
+        query  = window.location.search.substring(1);
+
+    urlParams = {};
+    while (match = search.exec(query))
+       urlParams[decode(match[1])] = decode(match[2]);
+})();
+
 window.onload=function(){
-	window.setTimeout(function(){
-		var c2d=$canvas.getContext("2d")
-		c2d.gcls("black")
-		//window.location.hash="3"
-		c2d.gputchr($canvas.width/2-((window.location.hash.length-1)*32/2),$canvas.height/2-(32/2),window.location.hash.substr(1),32,"red")
-	},1000)
+	var c2d=$canvas.getContext("2d")
+	c2d.gcls("black")
+	c2d.gputchr($canvas.width/2-(urlParams.text.length*32/2),$canvas.height/2-(32/2),urlParams.text,32,urlParams.color)
 }
