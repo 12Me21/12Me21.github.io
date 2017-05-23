188ebb9a767fbabd0a995134aacd03b8635fa666
12Me21 <12Me21.MC@gmail.com>
2017-05-23 15:32:21 -0400

    changed everything
    -now checks if global flag is set on regexes
    -renamed "class"/"className"/"classname"/"name" to "class" (this is the last time I promise)
    -RENAME ALL THE VARIABLES
    -doesn't create String.prototype.escapeHTML any more
    -no longer uses `class` since that's really new apparently
    -fixed spaces

--- rxhighlight/rxhighlight.js
+++ rxhighlight/rxhighlight.js
@@ -1,54 +1,59 @@
-class Highlighter{
-	constructor(find){
-		this.find=find.find;
-	}
-	
-	//apply highlighting to an HTML element
-	apply(codeElement){
-		var text=codeElement.textContent;
-		//find potential things to highlight
-		var list=[];
-		var fallback=0;
-		for(var i=0;i<this.find.length;i++){
-			var classname=this.find[i].classname;
-			var regex=this.find[i].regex;
-			var match;
-			
-			while(match=regex.exec(text)){
-				list.push({
-					start: match.index,
-					end: match.index+match[0].length,
-					classname: classname,
-					fallback: fallback++
-				});
-			}
-		}
-		
-		//highlight the correct ones
-		list=list.sort(function(a,b){
-			return a.start-b.start||b.end-a.end||a.fallback-b.fallback;
-		});
-		
-		var pos=0,output="";
-		for(var i=0;i<list.length;i++){
-			var word=list[i];
-			if (word.start>=pos){ //only highlight if it's past the end of the previous keyword
-				if (word.classname)
-					output+=text.substring(pos,word.start).escapeHTML()+'<span class="'+word.classname+'">'+text.substring(word.start, word.end).escapeHTML()+"</span>";
-				else
-					output+=text.substring(pos,word.end).escapeHTML();
-				pos=word.end;
-			}
-		}
-		output+=text.substring(pos).escapeHTML();
-		codeElement.innerHTML=output;
-	}
+function Highlighter (languageDefinition) {
+	for(var i = 0; i < languageDefinition.find.length; i++)
+		if (!languageDefinition.find[i].regex.global)
+			throw "Highlighter regex must have global flag set";
+	this.find = languageDefinition.find; //only this part is used currently
 }
 
-String.prototype.escapeHTML=(function(){
-	var converter=document.createElement("textarea");
-	return function(){
-		converter.textContent = this;
-		return converter.innerHTML;
+//apply highlighting to an HTML element
+Highlighter.prototype.apply = function (codeElement) {
+	var text = codeElement.textContent;
+	
+	//find potential things to highlight
+	var highlightList = [];
+	for (var i = 0; i < this.find.length; i++) {
+		var className = this.find[i].class;
+		var regex = this.find[i].regex;
+		//there really needs to be a better way get all matches:
+		var match;
+		while (match = regex.exec(text)) {
+			highlightList.push({
+				start: match.index,
+				end: match.index + match[0].length,
+				className: className,
+				index: highlightList.length
+			});
+		}
 	}
-})()
+	
+	highlightList = highlightList.sort(function (a,b) {
+		return a.start - b.start || b.end - a.end || a.index - b.index;
+	});
+	
+	//insert highlighting
+	var output = "";
+	var pos = 0;
+	for (var i = 0; i < highlightList.length; i++) {
+		var highlight = highlightList[i];
+		if (highlight.start >= pos) { //only highlight if it's past the end of the previous keyword
+			if (highlight.className)
+				output += Highlighter.escapeHTML(text.substring(pos, highlight.start)) +
+				          "<span class=\"" + highlight.className + "\">"+
+				          Highlighter.escapeHTML(text.substring(highlight.start, highlight.end)) +
+				          "</span>";
+			else
+				output += Highlighter.escapeHTML(text.substring(pos, highlight.end));
+			pos = highlight.end;
+		}
+	}
+	output += text.substring(pos).escapeHTML();
+	
+	codeElement.innerHTML = output;
+}
+
+//This should've been a builtin function, but oh well.
+Highlighter.converterElement = document.createElement("textarea");
+Highlighter.prototype.escapeHTML = function (text) {
+	this.converterElement.textContent = text;
+	return this.converterElement.innerHTML;
+}
