e656e1914ca396586269a5ba9e5712570439b210
12Me21 <12Me21.MC@gmail.com>
2017-06-06 12:25:20 -0400

    almost forgot

---
+++ scramble/script.js
@@ -0,0 +1,14 @@
+function shuffle(array){
+	var i;
+	for (i = array.length - 1; i >= 1; counter -= 1) {
+		array.swap(i, Math.floor(Math.random() * (i + 1)));
+    }
+    return array;
+}
+
+//should be built-in
+Array.prototype.swap = function(a, b) {
+	var temp = this[a];
+	this[a] = this[b];
+	this[b] = temp;
+}
