7fea7b8fba7a0cd909b1bf34df11b3865f7df149
12Me21 <12Me21.MC@gmail.com>
2017-06-07 12:32:04 -0400

    wow

--- kland_image/script.js
+++ kland_image/script.js
@@ -1,56 +1,61 @@
 //get image from file browser
 function imageUpload(element, callback) {
-   element.onchange = function() {
-        var reader = new FileReader()
-         reader.onload = function() {
-           
-            var image = new Image()
-            image.onload = callback
-            image.src = this.result
-      	}
-      	reader.readAsDataURL(this.files[0])
-	
-   }
+	"use strict";
+	element.onchange = function () {
+		var reader = new FileReader();
+		reader.onload = function () {
+			var image = new Image();
+			image.onload = callback;
+			image.src = this.result;
+		};
+		reader.readAsDataURL(this.files[0]);
+	};
 }
 
 //Convert image to blob PNG or JPEG depending on the size
-var toBlob = (function() {
-   var canvas = document.createElement("canvas")
-   return function(image, callback) {
-      if (image instanceof Blob) callback(image)
-      //draw on canvas
-      canvas.width = image.width
-      canvas.height = image.height
-      canvas.getContext("2d").drawImage(image, 0, 0)
-     if (!HTMLCanvasElement.prototype.toBlob) {
-	alert("Browser does not support canvas.toBlob.\nTry using 12Me21.github.io/kland_image/simple")
-	return
-	}
-      //get png blob
-      canvas.toBlob(function(blobPng) {
-         if (blobPng.size <= 750000) {
-            console.log("Converted to PNG")
-            callback(blobPng)
-         } else {
+var toBlob = (function () {
+	"use strict";
+	var canvas = document.createElement("canvas");
+	return function (image, callback) {
+		if (image.constructor === Blob) {
+			callback(image);
+			return;
+		}
+        //draw on canvas
+		canvas.width = image.width;
+		canvas.height = image.height;
+        canvas.getContext("2d").drawImage(image, 0, 0);
+        if (!HTMLCanvasElement.prototype.toBlob) {
+            alert("Browser does not support canvas.toBlob.\nTry using 12Me21.github.io/kland_image/simple");
+            return;
+        }
+        //get png blob
+        canvas.toBlob(function (blobPng) {
+            if (blobPng.size <= 750000) {
+                console.log("Converted to PNG");
+                callback(blobPng);
+                return;
+            }
             //get jpeg blob if png is too big
-            canvas.toBlob(function(blobJpeg) {
-               console.log("Converted to JPEG")
-               callback(blobJpeg)
-            }, "image/jpeg", 0.9)
-         }
-      })
-   }
-})()
+            canvas.toBlob(function (blobJpeg) {
+                console.log("Converted to JPEG");
+                callback(blobJpeg);
+                return;
+            }, "image/jpeg", 0.9);
+        });
+    };
+}());
 
 //upload image to kland
 function upload(blob, bucket, callback) {
-   //generate form data
-   var data = new FormData()
-   data.append("image", blob, blob.type == "image/jpeg" ? "blob.jpeg" : "blob.png")
-   data.append("bucket", bucket)
-   //send
-   var xhr = new XMLHttpRequest()
-   xhr.open("POST", "https://kland.smilebasicsource.com/uploadimage")
-   xhr.onload = callback
-   xhr.send(data)
+    "use strict";
+    //generate form data
+    var data = new FormData();
+    data.append("image", blob, "blob." + (blob.type === "image/jpeg" ? "jpg" : "png"));
+    data.append("bucket", bucket);
+    //send
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "https://kland.smilebasicsource.com/uploadimage");
+    xhr.onload = callback;
+    xhr.send(data);
 }
