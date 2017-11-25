5df18274d77ef737f6cbf6ef32736fd719b8258e
12Me21 <12Me21.MC@gmail.com>
2017-11-25 11:04:11 -0500

    hopefully easier to understand

--- sbhighlight2/sbhighlight.js
+++ sbhighlight2/sbhighlight.js
@@ -1,22 +1,25 @@
-var keywords=["BREAK","CALL","COMMON","CONTINUE","DATA","DEC","DEF","DIM","ELSE","ELSEIF","END","ENDIF","EXEC","FOR","GOSUB","GOTO","IF","INC","INPUT","LINPUT","NEXT","ON","OUT","PRINT","READ","REM","REPEAT","RESTORE","RETURN","STOP","SWAP","THEN","UNTIL","USE","VAR","WEND","WHILE",]
-var functions=["ABS","ACCEL","ACLS","ACOS","ARYOP","ASC","ASIN","ATAN","ATTR","BACKCOLOR","BACKTRACE","BEEP","BGANIM","BGCHK","BGCLIP","BGCLR","BGCOLOR","BGCOORD","BGCOPY","BGFILL","BGFUNC","BGGET","BGHIDE","BGHOME","BGLOAD","BGMCHK","BGMCLEAR","BGMCONT","BGMPAUSE","BGMPLAY","BGMPRG","BGMPRGA","BGMSET","BGMSETD","BGMSTOP","BGMVAR","BGMVOL","BGOFS","BGPAGE","BGPUT","BGROT","BGSAVE","BGSCALE","BGSCREEN","BGSHOW","BGSTART","BGSTOP","BGVAR","BIN$","BIQUAD","BQPARAM","BREPEAT","BUTTON","CALLIDX","CEIL","CHKCALL","CHKCHR","CHKFILE","CHKLABEL","CHKVAR","CHR$","CLASSIFY","CLIPBOARD","CLS","COLOR","CONTROLLER","COPY","COS","COSH","CSRX","CSRY","CSRZ","DATE$","DEG","DELETE","DIALOG","DISPLAY","DLCOPEN","DTREAD","EFCOFF","EFCON","EFCSET","EFCWET","ERRLINE","ERRNUM","ERRPRG","EXP","EXTFEATURE","FADE","FADECHK","FFT","FFTWFN","FILES","FILL","FLOOR","FONTDEF","FORMAT$","FREEMEM","GBOX","GCIRCLE","GCLIP","GCLS","GCOLOR","GCOPY","GFILL","GLINE","GLOAD","GOFS","GPAGE","GPAINT","GPRIO","GPSET","GPUTCHR","GSAVE","GSPOIT","GTRI","GYROA","GYROSYNC","GYROV","HARDWARE","HEX$","IFFT","INKEY$","INSTR","KEY","LEFT$","LEN","LOAD","LOCATE","LOG","MAINCNT","MAX","MICDATA","MICPOS","MICSAVE","MICSIZE","MICSTART","MICSTOP","MID$","MILLISEC","MIN","MPCOUNT","MPEND","MPGET","MPHOST","MPLOCAL","MPNAME$","MPRECV","MPSEND","MPSET","MPSTART","MPSTAT","OPTION","PCMCONT","PCMPOS","PCMSTOP","PCMSTREAM","PCMVOL","POP","POW","PRGDEL","PRGEDIT","PRGGET$","PRGINS","PRGNAME$","PRGSET","PRGSIZE","PRGSLOT","PROJECT","PUSH","RAD","RANDOMIZE","RENAME","RESULT","RGB","RGBREAD","RIGHT$","RINGCOPY","RND","RNDF","ROUND","RSORT","SAVE","SCROLL","SGN","SHIFT","SIN","SINH","SNDSTOP","SORT","SPANIM","SPCHK","SPCHR","SPCLIP","SPCLR","SPCOL","SPCOLOR","SPCOLVEC","SPDEF","SPFUNC","SPHIDE","SPHITINFO","SPHITRC","SPHITSP","SPHOME","SPLINK","SPOFS","SPPAGE","SPROT","SPSCALE","SPSET","SPSHOW","SPSTART","SPSTOP","SPUNLINK","SPUSED","SPVAR","SQR","STICK","STICKEX","STR$","SUBST$","SYSBEEP","TABSTEP","TALK","TALKCHK","TALKSTOP","TAN","TANH","TIME$","TMREAD","TOUCH","UNSHIFT","VAL","VERSON","VISIBLE","VSYNC","WAIT","WAVSET","WAVSETA","WIDTH","XOFF","XON","XSCREEN"]
-
-//I can't bring myself to use === and !==... they just look too silly lol
+var keywords=["BREAK","CALL","COMMON","CONTINUE","DATA","DEC","DEF","DIM","ELSE","ELSEIF","END","ENDIF","EXEC","FOR","GOSUB","GOTO","IF","INC","INPUT","LINPUT","NEXT","ON","OUT","PRINT","READ","REM","REPEAT","RESTORE","RETURN","STOP","SWAP","THEN","UNTIL","USE","VAR","WEND","WHILE"];
+var functions=["ABS","ACCEL","ACLS","ACOS","ARYOP","ASC","ASIN","ATAN","ATTR","BACKCOLOR","BACKTRACE","BEEP","BGANIM","BGCHK","BGCLIP","BGCLR","BGCOLOR","BGCOORD","BGCOPY","BGFILL","BGFUNC","BGGET","BGHIDE","BGHOME","BGLOAD","BGMCHK","BGMCLEAR","BGMCONT","BGMPAUSE","BGMPLAY","BGMPRG","BGMPRGA","BGMSET","BGMSETD","BGMSTOP","BGMVAR","BGMVOL","BGOFS","BGPAGE","BGPUT","BGROT","BGSAVE","BGSCALE","BGSCREEN","BGSHOW","BGSTART","BGSTOP","BGVAR","BIN$","BIQUAD","BQPARAM","BREPEAT","BUTTON","CALLIDX","CEIL","CHKCALL","CHKCHR","CHKFILE","CHKLABEL","CHKVAR","CHR$","CLASSIFY","CLIPBOARD","CLS","COLOR","CONTROLLER","COPY","COS","COSH","CSRX","CSRY","CSRZ","DATE$","DEG","DELETE","DIALOG","DISPLAY","DLCOPEN","DTREAD","EFCOFF","EFCON","EFCSET","EFCWET","ERRLINE","ERRNUM","ERRPRG","EXP","EXTFEATURE","FADE","FADECHK","FFT","FFTWFN","FILES","FILL","FLOOR","FONTDEF","FORMAT$","FREEMEM","GBOX","GCIRCLE","GCLIP","GCLS","GCOLOR","GCOPY","GFILL","GLINE","GLOAD","GOFS","GPAGE","GPAINT","GPRIO","GPSET","GPUTCHR","GSAVE","GSPOIT","GTRI","GYROA","GYROSYNC","GYROV","HARDWARE","HEX$","IFFT","INKEY$","INSTR","KEY","LEFT$","LEN","LOAD","LOCATE","LOG","MAINCNT","MAX","MICDATA","MICPOS","MICSAVE","MICSIZE","MICSTART","MICSTOP","MID$","MILLISEC","MIN","MPCOUNT","MPEND","MPGET","MPHOST","MPLOCAL","MPNAME$","MPRECV","MPSEND","MPSET","MPSTART","MPSTAT","OPTION","PCMCONT","PCMPOS","PCMSTOP","PCMSTREAM","PCMVOL","POP","POW","PRGDEL","PRGEDIT","PRGGET$","PRGINS","PRGNAME$","PRGSET","PRGSIZE","PRGSLOT","PROJECT","PUSH","RAD","RANDOMIZE","RENAME","RESULT","RGB","RGBREAD","RIGHT$","RINGCOPY","RND","RNDF","ROUND","RSORT","SAVE","SCROLL","SGN","SHIFT","SIN","SINH","SNDSTOP","SORT","SPANIM","SPCHK","SPCHR","SPCLIP","SPCLR","SPCOL","SPCOLOR","SPCOLVEC","SPDEF","SPFUNC","SPHIDE","SPHITINFO","SPHITRC","SPHITSP","SPHOME","SPLINK","SPOFS","SPPAGE","SPROT","SPSCALE","SPSET","SPSHOW","SPSTART","SPSTOP","SPUNLINK","SPUSED","SPVAR","SQR","STICK","STICKEX","STR$","SUBST$","SYSBEEP","TABSTEP","TALK","TALKCHK","TALKSTOP","TAN","TANH","TIME$","TMREAD","TOUCH","UNSHIFT","VAL","VERSON","VISIBLE","VSYNC","WAIT","WAVSET","WAVSETA","WIDTH","XOFF","XON","XSCREEN"];
 
 function highlight(code,callback){
-	var i=-1,c,isAlpha,isNum;
-	function scan(){
+	var i=-1,c,isAlpha,isDigit;
+	function next(){
 		i++;
 		c=code.charAt(i);
 		isAlpha=(c>='A'&&c<='Z'||c>='a'&&c<='z');
-		isNum=(c>='0'&&c<='9');
+		isDigit=(c>='0'&&c<='9');
+	}
+	
+	function jump(pos){
+		i=pos-1;
+		next();
 	}
 	
 	var prev=0;
 	function push(type){
 		var word=code.substring(prev,i);
 		prev=i;
-		if(type===true){
+		if(type=="word"){
 			var upper=word.toUpperCase();
 			
 			if(upper=="TO"||upper=="STEP"){
@@ -36,200 +39,240 @@ function highlight(code,callback){
 		callback(word,type);
 	}
 	
-	scan();
+	next();
 		
-	var start;
+	var start,start2;
+	//loop until the end of the string
 	while(c){
+		//
 		//keywords, functions, variables
+		//
 		if(isAlpha||c=='_'){
+			next();
 			//read name
-			do{
-				scan();
-			}while(isAlpha||isNum||c=='_');
-			//read type suffix
-			if(c=='#'||c=='%'||c=="$"){
-				scan();
+			while(isAlpha||isDigit||c=='_'){
+				next();
 			}
-			push(true);
+			//read type suffix
+			if(c=='#'||c=='%'||c=='$'){
+				next();
+			}
+			//push word type
+			push("word");
+		//
 		//numbers
-		}else if(isNum||c=='.'){
-			//read digits before decimal point
-			while(isNum){
-				scan();
+		//
+		}else if(isDigit||c=='.'){
+			//if digit was found, read all of them
+			while(isDigit){
+				next();
 			}
 			//if there's a decimal point
 			if(c=='.'){
+				next();
 				//read digits after
-				start=i;
-				do{
-					scan();
-				}while(isNum)
-				//if no digits
-				if(i==start+1){
-					//check for float type suffix
-					if(c=='#'){
-						scan();
+				if(isDigit){
+					next();
+					while(isDigit){
+						next();
 					}
-					//skip E notation
+				}else{
+					//if GOTO is available: GOTO @skip_e
+					if(c=='#'){
+						next();
+					}
 					push("number");
 					continue;
 				}
 			}
 			//E notation
 			if(c=='E'||c=='e'){
-				start=i;
+				var ePos=i;
+				next();
 				//check for + or -
-				scan();
 				if(c=='+'||c=='-'){
-					scan()
+					next();
 				}
 				//read digits
-				var start2=i;
-				while(isNum){
-					scan();
-				}
-				//if no digits (invalid)
-				if(i==start2){
-					i=start-1;
-					scan();
+				if(isDigit){
+					next();
+					while(isDigit){
+						next();
+					}
+				//no digits (invalid)
+				}else{
+					jump(ePos);
 					push();
 					continue;
 				}
 			}
+			//(if GOTO is available: @skip_e)
 			//read float suffix
 			if(c=='#'){
-				scan();
+				next();
 			}
 			push("number");
+		//
 		//strings
+		//
 		}else switch(c){
 		case '"':
+			next();
 			//read characters until another quote, line ending, or end of input
-			do{
-				scan();
-			}while(c && c!='"' && c!='\n' && c!='\r')
+			while(c && c!='"' && c!='\n' && c!='\r'){
+				next();
+			}
 			//read closing quote
 			if(c=='"'){
-				scan()
+				next();
 			}
-			push("string")
+			push("string");
+		//
 		//comments
+		//
 		break;case '\'':
+			next();
 			//read characters until line ending or end of input
-			do{
-				scan();
-			}while(c&&c!='\n'&&c!='\r')
+			while(c && c!='\n' && c!='\r'){
+				next();
+			}
 			push("comment");
-		//logical and, hexadecimal, binary
+		//
+		//logical AND, hexadecimal, binary
+		//
 		break;case '&':
-			scan();
+			next();
 			switch(c){
 			//logical and
 			case '&':
-				scan();
+				next();
 				push("operator");
 			//hexadecimal
 			break;case 'H':case 'h':
+				var hPos=i;
+				next();
 				//read hexadecimal digits
-				start=i;
-				do{
-					scan();
-				}while(isNum||c>='A'&&c<='F'||c>='a'&&c<='f')
-				//if no digits found
-				if(i==start+1){
-					i=start-1;
-					scan();
-					push();
-				//valid number
-				}else{
+				if(isDigit||c>='A'&&c<='F'||c>='a'&&c<='f'){
+					next();
+					while(isDigit||c>='A'&&c<='F'||c>='a'&&c<='f'){
+						next();
+					}
 					push("number");
+				}else{
+					jump(hPos);
+					push();
 				}
 			//binary
 			break;case 'B':case 'b':
-				//read binary digits
-				start=i;
-				do{
-					scan();
-				}while(c=='0'||c=='1')
-				//if no digits
-				if(i==start+1){
-					i=start-1;
-					scan();
-					push();
-				//valid number
-				}else{
+				var bPos=i;
+				next();
+				//read hexadecimal digits
+				if(c=='0'||c=='1'){
+					next();
+					while(c=='0'||c=='1'){
+						next();
+					}
 					push("number");
+				}else{
+					jump(bPos);
+					push();
 				}
 			//invalid &
 			break;default:
 				push();
 			}
+		//
 		//labels
+		//
 		break;case '@':
-			do{
-				scan();
-			}while(isNum||isAlpha||c=='_')
+			next();
+			//read name
+			while(isDigit||isAlpha||c=='_'){
+				next();
+			}
+			//ok
 			push("label");
+		//
 		//constants
+		//
 		break;case '#':
-			start=i;
-			do{
-				scan();
-			}while(isNum||isAlpha)
-			if(i==start+1){
-				push();
-			}else{
+			next();
+			//read name
+			if(isDigit||isAlpha){
+				next();
+				while(isDigit||isAlpha){
+					next();
+				}
 				push("number");
-			}
-		//logical or
-		break;case '|':
-			scan();
-			if(c=='|'){
-				scan();
-				push("operator");
 			}else{
 				push();
 			}
+		//
+		//logical or
+		//
+		break;case '|':
+			next();
+			//logical or
+			if(c=='|'){
+				next();
+				push("operator");
+			//invalid
+			}else{
+				push();
+			}
+		//
 		//less than, less than or equal, left shift
+		//
 		break;case '<':
-			scan();
+			next();
+			//<= <<
 			if(c=='='||c=='<'){
-				scan();
+				next();
 			}
 			push("operator");
+		//
 		//greater than, greater than or equal, right shift
+		//
 		break;case '>':
-			scan();
+			next();
+			//>= >>
 			if(c=='='||c=='>'){
-				scan();
+				next();
 			}
 			push("operator");
-		//set, equal
+		//
+		//equal, equal more
+		//
 		break;case '=':
-			scan();
+			next();
 			//==
 			if(c=='='){
-				scan();
+				next();
 				push("operator");
 			}else{
-				push();
 				push("equals");
 			}
+		//
 		//logical not, not equal
+		//
 		break;case '!':
-			//check for =
-			scan();
+			next();
+			// !=
 			if(c=='='){
-				scan();
+				next();
 			}
 			push("operator");
+		//
 		//add, subtract, multiply, divide
+		//
 		break;case '+':case '-':case '*':case '/':
-			scan();
+			next();
 			push("operator");
+		//
 		//other
+		//
 		break;default:
-			scan();
+			next();
 			push();
 		}
 	}
