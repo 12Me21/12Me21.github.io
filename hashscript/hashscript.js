d884fb052e0ff545e51e6782a3283b8834e08a2e
12Me21 <12Me21.MC@gmail.com>
2018-04-21 16:08:58 -0400

    fix auth key parameter

--- hashscript/hashscript.js
+++ hashscript/hashscript.js
@@ -37,7 +37,7 @@ function runHash(hash){
 						break;case "S":
 							data=data.replace(/\\/g,"¥").replace(/\u007F/g,"\\");
 						break;case "P": //hacked together paste.ee loader
-							data=JSON.parse(syncLoad("https://api.paste.ee/v1/pastes/"+data+"&key=agEhowbhiShxMFYgezNWDUxEbhW3gLFwhWbv7lYhm"));
+							data=JSON.parse(syncLoad("https://api.paste.ee/v1/pastes/"+data+"?key=agEhowbhiShxMFYgezNWDUxEbhW3gLFwhWbv7lYhm"));
 							assert(data.success,"paste.ee load failed "+data.error);
 							assert(data.paste && data.paste.sections && data.paste.sections[0] && data.paste.sections[0].content,"too complex");
 							data=data.paste.sections[0].content;
