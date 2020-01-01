8bef2e642cd318481349297be20c4708ec974cb4
12Me21 <12Me21.MC@gmail.com>
2020-01-01 17:14:07 -0500

    updated

--- 2qsp_modem/script.js
+++ 2qsp_modem/script.js
@@ -1,143 +1,112 @@
-/*
- * Petitcom 3 QPSK Modulater
- * Copyright (C) 2014 OBN-soft
- * Version: 0.2.1
- * LastModified: Nov 27 2014
- * 
- * This software is released under the MIT License.
- * http://opensource.org/licenses/mit-license.php
- * 
- */
+var SAMPLE_RATE = 32728;
+var SYMBOL_SIZE = 8;
+var FULL_WAVE = new Uint8Array([10, 79,176,245,245,176, 79, 10, 10, 79,176,245,245,176, 79, 10]);
+var SYMBOLS = [];
+for(var i=0;i<4;i++){
+	SYMBOLS[i] = FULL_WAVE.subarray(i*(SYMBOL_SIZE/4), i*(SYMBOL_SIZE/4)+SYMBOL_SIZE);
+}
+var SYNC = create_sync(3);
 
-var elmDivInfo;
-var elmAudioPlay;
-
-var SYMBOL = 8;
-
-var samples = String.fromCharCode(10, 79,176,245,245,176, 79, 10, 10, 79,176,245,245,176, 79, 10);
-
-function setDivElementForInfo(element) {
-	elmDivInfo = element;
+function make_audio(data) {
+	var samples = new Uint8Array(SYNC.length + data.length*4*SYMBOL_SIZE);
+	samples.set(SYNC, 0);
+	modulate(samples.subarray(SYNC.length), data);
+	return make_wav_url(SAMPLE_RATE, 1, 1, samples);
 }
 
-function setAudioElementForData(element) {
-	elmAudioPlay = element;
-}
-
-/*--------*/
-
-window.sampleRate=32728;
-window.syncsize=10000;
-
-function fileOpen(evt) {
-	elmAudioPlay.src = null;
-	displayInfo(null);
-	var files = evt.target.files;
-	if (files.length == 0) return;
-	var file = files[0];
+function file_upload(event) {
+	display(null);
+	var files = event.target.files;
+	if (!files[0])
+		return;
 	var reader = new FileReader();
-	reader.onload = function(evt) {
-		var data = reader.result;
-		displayInfo(data);
-		setTimeout(function(){
-			var signals = createSyncSignal() + createDataSignal(data);
-			signals += String.fromCharCode(0x80).repeat(10000);
-			var wavefile = make_wav(window.sampleRate, 1, 1, signals);
-			elmAudioPlay.src = "data:audio/wav;base64," + btoa(wavefile);
-		},0);
+	reader.onload = function(event) {
+		var data = new Uint8Array(reader.result);
+		display(data);
 	}
-	reader.readAsBinaryString(file);
+	reader.readAsArrayBuffer(files[0]);
 }
 
-/*--------*/
-
-function displayInfo(data) {
-	var i, text;
-	for (i = elmDivInfo.childNodes.length - 1; i >= 0; i--) {
-		elmDivInfo.removeChild(elmDivInfo.childNodes[i]);
-	}
-
-	text = "Data size: ";
-	if (data) text += data.length + " bytes";
-	appendInfo(text);
-	
-	text = "Time: ";
+function display(data) {
 	if (data) {
-		var sec = data.length*4/(32728/SYMBOL);
-		text += Math.floor(sec/60) + ":" + (100+Math.round(sec % 60)).toString().substr(1);
+		$size.textContent = data.length + " bytes";
+		$checksum.textContent = checksum(data);
+		$audio.src = make_audio(data);
+	} else {
+		$size.textContent = "";
+		$checksum.textContent = "";
+		$audio.src = null;
 	}
-	appendInfo(text);
-
-	text = "Checksum: ";
-	if (data) {
-		var sum = 0;
-		for (i = 0; i < data.length; i++) {
-			sum += data.charCodeAt(i)//( ^ i * 23) & 0xFF;
-		}
-		text += sum.toString(16).toUpperCase();
-	}
-	appendInfo(text);
-
 }
 
-function appendInfo(text) {
-	elmDivInfo.appendChild(document.createTextNode(text));
-	elmDivInfo.appendChild(document.createElement("br"));
+// (this is a very bad checksum, but I needed to save space in the receiver)
+function checksum(data) {
+	var sum = 0;
+	for (var i=0; i<data.length; i++)
+		sum += data[i];
+	return (sum|0).toString(16).toUpperCase(); // HEX$
 }
 
-/*--------*/
-
-function createSyncSignal() {
-	var i;
-	var signals = String.fromCharCode(80).repeat(10);
-
-	for (i = 0; i < syncsize; i++) {
-		signals += samples.substr(0, SYMBOL);
+function create_sync(seconds) {
+	var cycles = Math.ceil(seconds * SAMPLE_RATE / SYMBOL_SIZE);
+	var samples = new Uint8Array(100+SYMBOL_SIZE*cycles+SYMBOL_SIZE);
+	for (var i=0; i<100; i++) {
+		samples[i] = 0x80;
 	}
-	signals += samples.substr(2, SYMBOL);
-	return signals;
+	for (i=0; i<cycles; i++)
+		samples.set(SYMBOLS[0], 100+i*SYMBOL_SIZE);
+	samples.set(SYMBOLS[1], 100+i*SYMBOL_SIZE);
+	return samples;
 }
 
-function createDataSignal(data) {
-	var i, j, val, rs_val;
-	var len = data.length;
-	var signals = "";
-
-	for (i = 0; i < len; i++) {
-		val = data.charCodeAt(i)
-		for(j=0;j<8;j+=2){
-			signals += samples.substr((val>>j & 3)*2, SYMBOL);
+function modulate(samples, data) {
+	var pos = 0;
+	for (var i=0; i<data.length; i++) {
+		var val = data[i];
+		for (var j=0; j<8; j+=2) {
+			samples.set(SYMBOLS[val>>j & 3], pos);
+			pos += SYMBOL_SIZE;
 		}
 	}
-	return signals;
 }
 
-function i32(x) {
-	return String.fromCharCode(x&0xFF, x>>8&0xFF, x>>16&0xFF, x>>24&0xFF);
-}
-function i16(x) {
-	return String.fromCharCode(x&0xFF, x>>8&0xFF);
+function put_str(a, o, s){
+	for(var i=0;i<s.length;i++)
+		a[o+i]=s.charCodeAt(i);
 }
 
-function make_wav(samplerate, channels, samplesize, data){
-	var numsamples = data.length / samplesize / channels;
-	
-	var data_chunk = "data";
-	data_chunk += i32(numsamples * samplesize * channels);
-	data_chunk += data;
-	
-	var fmt_chunk = "fmt "; //subchunk label
-	fmt_chunk += i32(16); //subchunk size
-	fmt_chunk += i16(1); //audio format
-	fmt_chunk += i16(channels); // number of channels
-	fmt_chunk += i32(samplerate); //samples per second
-	fmt_chunk += i32(samplerate * channels * samplesize); //bytes per second (all channels)
-	fmt_chunk += i16(channels * samplesize); //bytes per sample (all channels)
-	fmt_chunk += i16(8 * samplesize); //bits per sample
-	
-	var riff_chunk = "RIFF";
-	riff_chunk += i32(fmt_chunk.length + data_chunk.length + 4);
-	riff_chunk += "WAVE";
-	
-	return riff_chunk + fmt_chunk + data_chunk;
+function put_i32(a, o, i){
+	a[o  ] = i    &0xFF;
+	a[o+1] = i>> 8&0xFF;
+	a[o+2] = i>>16&0xFF;
+	a[o+3] = i>>24&0xFF;
+}
+
+function put_i16(a, o, i){
+	a[o  ] = i    &0xFF;
+	a[o+1] = i>> 8&0xFF;
+}
+
+function make_wav_url(samplerate, channels, samplesize, data) {
+	var wav = new Uint8Array(4+4+4 + 4+4+16 + 4+4+data.byteLength);
+	// RIFF chunk
+	put_str(wav,0,"RIFF"); // RIFF label
+	put_i32(wav,4, 4 + 4+4+16 + 4+4+data.byteLength); // size (including subchunks)
+	put_str(wav,8,"WAVE"); // type or whatever
+	// fmt subchunk
+	put_str(wav,12,"fmt "); //subchunk label
+	put_i32(wav,16,16); //subchunk size
+	put_i16(wav,20,1); //audio format
+	put_i16(wav,22,channels); // number of channels
+	put_i32(wav,24,samplerate); //samples per second
+	put_i32(wav,28,samplerate * channels * samplesize); //bytes per second (all channels)
+	put_i16(wav,32,channels * samplesize); //bytes per sample (all channels)
+	put_i16(wav,34,8 * samplesize); //bits per sample
+	// data subchunk
+	put_str(wav,36,"data");
+	put_i32(wav,40,data.byteLength);
+	wav.set(data,44);
+	var x = URL.createObjectURL(new Blob([wav.buffer],{type:"audio/wav"}));
+	console.log("ok?", x);
+	return x
 }
